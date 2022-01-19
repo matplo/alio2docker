596499d7b234df6f0c1b443afb9e514818ad4cbf
matplo <ploskon@gmail.com>
2022-01-18 22:49:20 -0800

    check before starting

--- removeExitedContainers.sh
+++ removeExitedContainers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker rm $(docker ps --filter status=exited -q)
+# docker rm $(docker ps --filter status=exited -q)
 
 # or
 
--- runDocker.sh
+++ runDocker.sh
@@ -1,10 +1,87 @@
 #!/bin/bash
 
-docker run -it \
-	--mount type=bind,source="$(pwd)",target=/alisoft \
-	-w /alisoft -h alio2dock --env-file alio2dockenvs.sh \
-	alisoft:o2 
-	
-	#\
-	#/bin/bash 
-#	/alisoft/alidocko2shell.sh
+savedir=${PWD}
+
+function thisdir()
+{
+	SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo ${DIR}
+}
+THISD=$(thisdir)
+source ${THISD}/util.sh
+separator "${BASH_SOURCE}"
+
+echo_info "Checking for exited containers..."
+exited_imgs=( $(docker ps --filter status=exited | awk '{print $1,$2}') )
+running_imgs=( $(docker ps --filter status=running | awk '{print $1,$2}') )
+any_imgs=( $(docker ps -all | awk '{print $1,$2}') )
+# echo ${exited_imgs[@]}
+
+shash=""
+for em in ${exited_imgs[@]}
+do
+	if [ "x${em}" == "xalisoft:o2" ]; then
+		runlistExited="${runlistExited} ${shash}"
+	fi
+	shash=${em}
+done
+nrunlistExited=$(echo ${runlistExited} | wc -w | tr -d ' ')
+echo_info "Found $nrunlistExited exited containers."
+
+shash=""
+for em in ${running_imgs[@]}
+do
+	if [ "x${em}" == "xalisoft:o2" ]; then
+		runlistRunning="${runlistRunning} ${shash}"
+	fi
+	shash=${em}
+done
+nrunlistRunning=$(echo ${runlistRunning} | wc -w | tr -d ' ')
+echo_info "Found $nrunlistRunning running containers."
+
+shash=""
+for em in ${any_imgs[@]}
+do
+	if [ "x${em}" == "xalisoft:o2" ]; then
+		runlistAny="${runlistAny} ${shash}"
+	fi
+	shash=${em}
+done
+nrunlistAny=$(echo ${runlistAny} | wc -w | tr -d ' ')
+echo_info "Found $nrunlistAny any containers."
+
+if [ "x0" == "x$nrunlistExited" ]; then
+	separator "Checking for running containers..."
+	docker ps --filter status=running
+	echo_info "Found ${nrunlistRunning} running containers..."
+	if [ "x0" == "x$nrunlistRunning" ]; then
+		separator "Executing docker run..."
+		docker run -it \
+		--mount type=bind,source="$(pwd)",target=/alisoft \
+		-w /alisoft -h alio2dock --env-file alio2docker.env \
+		--name alio2 \
+		alisoft:o2 
+	else
+		if [ "x1" == "x$nrunlistRunning" ]; then
+			separator "Attaching to ${runlistRunning} ..."
+			docker container attach ${runlistRunning}
+		else
+			echo_error "Multiple exited containers... unclear what to do... stop here."
+		fi
+	fi
+else
+	if [ "x1" == "x$nrunlistExited" ]; then
+		separator "Executing docker start on ${runlistExited} ..."
+		docker start ${runlistExited}
+		docker container attach ${runlistExited}
+	else
+		echo_error "Multiple exited containers... unclear what to do... stop here."
+		docker ps --filter status=exited
+	fi
+fi
